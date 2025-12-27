7b4bb4391e8d481e89c576b5021174afe8c1c955
Tuan Nguyen <Tuan Nguyen>
2025-12-26 18:18:57 -0600

    chore: intial commits

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,27 @@
+package com.example.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet("styles.css")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
